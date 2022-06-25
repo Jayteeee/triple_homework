ba709d4c841e3762f2f9e7f89aaf9e90109dad98
Jayteeee <wjdxo9928@gmail.com>
2022-06-25 14:26:07 +0900

    CHORE: 주석 제거

--- src/components/Description.js
+++ src/components/Description.js
@@ -8,15 +8,12 @@ const Description = () => {
     const handle = setInterval(() => {
       $counter.innerHTML = Math.ceil(max - now);
 
-      // 목표수치에 도달하면 정지
       if (now < 0) {
         clearInterval(handle);
       }
 
-      // 증가되는 값이 계속하여 작아짐
       const step = now / 20;
 
-      // 값을 적용시키면서 다음 차례에 영향을 끼침
       now -= step;
     }, 20);
   };
